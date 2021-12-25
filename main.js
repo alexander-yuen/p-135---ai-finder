13b8b3a8c86ad424150626354cf74ec3dffae2aa
alexander-yuen <79123774+alexander-yuen@users.noreply.github.com>
2021-12-24 20:50:11 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -20,9 +20,6 @@ function start(){
 function modelLoaded(){
     console.log("Model Loaded");
     status = true;
-    video.loop();
-    video.speed(1);
-    video.volume(0);
 }
 
 function gotResult(error, results){
@@ -38,5 +35,32 @@ function draw(){
         if(status != "")
         {
             console.log("Status Detecting Objects");
+            objectDetector.detect(video, gotResult);
+            for (i = 0; i < objects.length; i++){
+                document.getElementById("status").innerHTML = "Status : Objects Detected";
+                document.getElementById("number_of_objects").innerHTML = "Number of Objects detected are : "+ objects.length;
+
+                fill("#FF0000");
+                percent = floor(objects[i].confidence * 100);
+                text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+                noFill();
+                stroke("#FF0000");
+                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+
+                if(objects[i].label == object_name)
+                {
+                    
+                    objectDetector.detect(gotResult);
+                    document.getElementById("object_status").innerHTML = object_name + " Found";
+                    synth = window.speechSynthesis;
+                    utterThis = new SpeechSynthesisUtterance(object_name + "Found");
+                
+                }
+                else
+                {
+                    document.getElementById("object_status").innerHTML = object_name + " Not Found";
+                }
+            }
         }
 }
